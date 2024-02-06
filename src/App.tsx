4e96ab8d7ed93f6088e65b52852087b2bceded10
jyotiraditya <jyotiraditya24@gmail.com>
2024-02-06 19:59:06 +0530

    react router dom implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+const Customers = lazy(() => import("./pages/Customers"));
+
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<>Hi</>}></Route>
-        <Route path="/test" element={<>Hello</>}></Route>
-      </Routes>
+      <Suspense fallback="loadding......">
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />}></Route>
+          <Route path="/admin/products" element={<Products />}></Route>
+          <Route path="/admin/transaction" element={<Transaction />}></Route>
+          <Route path="/admin/transaction" element={<Customers />}></Route>
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
