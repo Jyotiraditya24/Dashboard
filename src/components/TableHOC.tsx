85a64cf73cdf02f8e8990fc091906a387be077e4
jyotiraditya <jyotiraditya24@gmail.com>
2024-02-18 23:19:15 +0530

    making table

--- src/components/TableHOC.tsx
+++ src/components/TableHOC.tsx
@@ -1,7 +1,44 @@
-const TableHOC = () => {
-  return (
-    <div>TableHOC</div>
-  )
-}
+import { useTable, Column, TableOptions } from "react-table";
 
-export default TableHOC
+const TableHOC = <T extends Object>(
+  columns: Column<T>[],
+  data: T[],
+  containerClassName: string,
+  heading: string
+) => {
+  return function HOC() {
+    const options: TableOptions<T> = {
+      columns,
+      data,
+    };
+    const table = useTable(options);
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+      table; 
+    return (
+      <div className={containerClassName}>
+        <h2 className="heading">{heading}</h2>
+        <table className="table" {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderGroupProps}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+
+            <tbody {...getTableBodyProps()}>
+                  {
+                    rows.map((r))
+                  }
+            </tbody>
+          </thead>
+        </table>
+      </div>
+    );
+  };
+};
+
+export default TableHOC;
