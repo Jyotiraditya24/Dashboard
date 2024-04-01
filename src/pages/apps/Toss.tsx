c4daf7cc0ec393bd58ae9a7c0e09c405b3c9c1cc
jyotiraditya <jyotiraditya24@gmail.com>
2024-04-01 16:08:35 +0530

    Toss coin done

--- src/pages/apps/Toss.tsx
+++ src/pages/apps/Toss.tsx
@@ -1,12 +1,37 @@
-import Sidebar from "../../components/Sidebar"
-
+import { useState } from "react";
+import Sidebar from "../../components/Sidebar";
 
 const Toss = () => {
+  const [angle, setAngle] = useState<number>(0);
+  const flipCoin = () => {
+    if (Math.random() > 0.5) {
+      setAngle((prev) => prev + 180);
+    } else {
+      setAngle((prev) => prev + 360);
+    }
+    console.log("Hi");
+  };
+
   return (
     <div className="adminContainer">
       <Sidebar></Sidebar>
-      </div> 
-  )
-}
+      <main className="dashboard-app-container">
+        <h1>Toss</h1>
+        <section>
+          <article
+            className="tosscoin"
+            onClick={flipCoin}
+            style={{
+              transform: `rotateY(${angle}deg)`,
+            }}
+          >
+            <div></div>
+            <div></div>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
+};
 
-export default Toss
+export default Toss;
