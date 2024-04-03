a992ab3d3f9f9d424deb10f28357483ce7f479c8
jyotiraditya <jyotiraditya24@gmail.com>
2024-04-03 15:05:28 +0530

    Responsive almost done

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,16 @@
+import { useEffect, useState } from "react";
 import { IconType } from "react-icons";
-import { Link, Location, useLocation } from "react-router-dom";
-import {
-  RiDashboardFill,
-  RiShoppingBag3Fill,
-  RiCoupon3Fill,
-} from "react-icons/ri";
 import { AiFillFileText } from "react-icons/ai";
+import {
+  FaChartBar, FaChartLine, FaChartPie, FaGamepad, FaStopwatch
+} from "react-icons/fa";
+import { HiMenuAlt4 } from "react-icons/hi";
 import { IoIosPeople } from "react-icons/io";
 import {
-  FaChartBar,
-  FaChartPie,
-  FaChartLine,
-  FaStopwatch,
-  FaGamepad,
-} from "react-icons/fa";
+  RiCoupon3Fill, RiDashboardFill,
+  RiShoppingBag3Fill
+} from "react-icons/ri";
+import { Link, Location, useLocation } from "react-router-dom";
 
 interface ListProps {
   url: string;
@@ -126,55 +123,94 @@ const DashboardData_3: {
 
 const Sidebar = () => {
   const location = useLocation();
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [phoneActive, setPhoneActive] = useState<boolean>(
+    window.innerWidth < 1100
+  );
+  function resizeHandler() {
+    setPhoneActive(window.innerWidth < 1100);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
   return (
-    <aside>
-      <h2>Logo</h2>
-      {/* DIV 1 */}
-      <div>
-        <h5>Dashboard</h5>
-        <ul>
-          {DashboardData_1.map((data) => (
-            <List
-              key={data.id}
-              url={data.url}
-              text={data.text}
-              location={location}
-              Icon={data.icon}
-            />
-          ))}
-        </ul>
-      </div>
-      {/* DIV 2 */}
-      <div>
-        <h5>Charts</h5>
-        <ul>
-          {DashboardData_2.map((data) => (
-            <List
-              key={data.id}
-              url={data.url}
-              text={data.text}
-              location={location}
-              Icon={data.icon}
-            />
-          ))}
-        </ul>
-      </div>
-      {/* DIV 3 */}
-      <div>
-        <h5>Apps</h5>
-        <ul>
-          {DashboardData_3.map((data) => (
-            <List
-              key={data.id}
-              url={data.url}
-              text={data.text}
-              location={location}
-              Icon={data.icon}
-            />
-          ))}
-        </ul>
-      </div>
-    </aside>
+    <>
+      {phoneActive && (
+        <button id="hamburger" onClick={() => setShowModal((prev) => !prev)}>
+          <HiMenuAlt4 />
+        </button>
+      )}
+
+      <aside
+        style={
+          phoneActive
+            ? {
+                width: "20rem",
+                height: "100rem",
+                position: "fixed",
+                top: 0,
+                left: showModal ? "0rem" : "-20rem",
+                transition: "all 0.5s",
+              }
+            : {}
+        }
+      >
+        <h2>Logo</h2>
+        {/* DIV 1 */}
+        <div>
+          <h5>Dashboard</h5>
+          <ul>
+            {DashboardData_1.map((data) => (
+              <List
+                key={data.id}
+                url={data.url}
+                text={data.text}
+                location={location}
+                Icon={data.icon}
+              />
+            ))}
+          </ul>
+        </div>
+        {/* DIV 2 */}
+        <div>
+          <h5>Charts</h5>
+          <ul>
+            {DashboardData_2.map((data) => (
+              <List
+                key={data.id}
+                url={data.url}
+                text={data.text}
+                location={location}
+                Icon={data.icon}
+              />
+            ))}
+          </ul>
+        </div>
+        {/* DIV 3 */}
+        <div>
+          <h5>Apps</h5>
+          <ul>
+            {DashboardData_3.map((data) => (
+              <List
+                key={data.id}
+                url={data.url}
+                text={data.text}
+                location={location}
+                Icon={data.icon}
+              />
+            ))}
+          </ul>
+        </div>
+        {phoneActive && (
+          <button id="close-sidebar" onClick={() => setShowModal(false)}>
+            Close
+          </button>
+        )}
+      </aside>
+    </>
   );
 };
 
